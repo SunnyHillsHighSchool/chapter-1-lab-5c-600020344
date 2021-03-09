caef61bf93332905ac811f1d3dc7019a9b161423
600020344 <77069279+600020344@users.noreply.github.com>
2021-03-08 20:34:40 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,27 +1,13 @@
-//� A+ Computer Science  -  www.apluscompsci.com
+//A+ Computer Science - www.apluscompsci.com
 //Name -
-//Date -  
+//Date -
 //Class -
-//Lab  -
-
-import static java.lang.System.*;
-import java.awt.Color;
-
+//Lab -
+import javax.swing.JFrame;
 public class Main
 {
-	public static void main(String[] args)
-	{
-		Piece pieceOne = new Piece();
-		out.println(pieceOne);
-		
-		Piece redChecker = new Piece("red checker");
-		out.println(redChecker);	
-		
-		Piece theShoe = new Piece(100,100,"the shoe from a money game");
-		out.println(theShoe);	
-		
-		Piece bishop = new Piece(200 ,200, 20, 20, "bishop", Color.WHITE);
-		out.println(bishop);					
-	}
+  public static void main( String args[] )
+  {
+    TicTacToe run = new TicTacToe();
+  }
 }
-
